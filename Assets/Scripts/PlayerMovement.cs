7b10375139f255f6bbff60c58838010e9e86b7fd
Munski95 <38910596+Munski95@users.noreply.github.com>
2018-09-18 10:31:18 +0300

    Add files via upload

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float moveSpeed;
+    public float jumpSpeed;
+
+    private void FixedUpdate()
+    {
+        if (Input.GetButton("Vertical"))
+        {
+            transform.localPosition += Input.GetAxis("Vertical") * transform.forward * Time.deltaTime * moveSpeed;
+        }
+
+        if (Input.GetButtonDown("Jump") && transform.position.y <= 0.5)
+        {
+            transform.GetComponent<Rigidbody2D>().AddForce(Vector2.up * jumpSpeed);
+        }
+    }
+}
