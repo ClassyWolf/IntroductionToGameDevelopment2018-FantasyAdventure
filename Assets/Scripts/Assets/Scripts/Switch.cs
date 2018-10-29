087ea162745456e35ba7bdbdb7afd799ac04d4c0
raven1934 <43270705+raven1934@users.noreply.github.com>
2018-10-29 12:56:57 +0200

    Add files via upload
    6 prefabs, cratespawner, healingcrate, crate, breakablecrate, spikes, switch and enemy added 6 different scripts for the functionalities aswell.

---
+++ Assets/Scripts/Assets/Scripts/Switch.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour {
+
+    public bool isActivated;
+    
+    void start()
+    {
+        isActivated = false;
+    }
+	
+    void OnTriggerEnter2D(Collider2D colliderObject)
+    {
+        if (colliderObject.tag=="Player" && isActivated==false)
+        {
+            isActivated = true;
+
+            Vector3 scale = this.transform.localScale;
+            this.transform.localScale = new Vector3(-scale.x,scale.y,scale.z);
+
+            print("hit");
+
+        }
+    }
+
+}
