aedd9c45e739965bbd781bd5e2d13c1eb4fe7473
raven1934 <43270705+raven1934@users.noreply.github.com>
2018-11-03 17:23:08 +0200

    Add files via upload
    Included the readme for the changes that took place on this patch, feel free to contact me in case something is out of order. In case such event happens ill be dragging the project also to school in usb-drive so the event with last presentation won't take place again.

---
+++ Assets/Scripts/HealthUpdate.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(PolygonCollider2D))]
+
+public class HealthUpdate : MonoBehaviour
+{
+
+    //remember to use the trigger object inside the visual/physical object as it is not meant to be physical originally
+
+    [SerializeField] private int healthChangeAmount;
+    [SerializeField] private bool destroyOnContact;
+
+    void OnTriggerStay2D(Collider2D colliderObject)
+    {
+
+        if (colliderObject.tag == "Player" || colliderObject.tag == "Enemy")
+        {
+
+            colliderObject.GetComponent<CharacterStatus>().UpdateHealth(healthChangeAmount);
+            if (destroyOnContact == true)
+            {
+                if (transform.parent != null)
+                {
+                    Destroy(transform.parent.gameObject); //destroys the parent object aswell
+                }
+                else
+                {
+                    Destroy(gameObject);
+                }
+            }
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/ShatterOnImpact.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShatterOnImpact : MonoBehaviour
+{
+
+    [SerializeField] private float maximumForce;
+    private Vector2 lastSpeed;
+    private new Rigidbody2D rigidbody;
+
+    void Start()
+    {
+
+        rigidbody = GetComponent<Rigidbody2D>();
+
+        if (maximumForce == 0) { maximumForce = Physics2D.gravity.magnitude*rigidbody.mass; }
+
+    }
+
+    void FixedUpdate()
+    {
+
+        float appliedForce = (rigidbody.velocity-lastSpeed).magnitude*rigidbody.mass;
+
+        print(appliedForce);
+
+        if (appliedForce > maximumForce)
+        {
+
+            Destroy(gameObject);
+
+        }
+
+        else
+        {
+
+            lastSpeed = rigidbody.velocity;
+
+        }
+
+    }
+
+}
