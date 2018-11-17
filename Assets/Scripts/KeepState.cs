53f71c1c62b95a42a19773f18ca040b790c5e7f7
raven1934 <43270705+raven1934@users.noreply.github.com>
2018-11-17 21:54:27 +0200

    Keepstate functionality
    Enables objects to exist after the level is reloaded or removed/destroyed.

---
+++ Assets/Scripts/KeepState.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeepState : MonoBehaviour {
+
+    private static GameObject undestroyableObject;
+
+    void Awake () {
+
+        if (undestroyableObject == null)
+        {
+            undestroyableObject = this.gameObject;
+            DontDestroyOnLoad(undestroyableObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+    }
+	
+}
